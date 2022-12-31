7427fbae49b5174a13fe4a353233a6a065bc7cbc
codeskyworld <codeskyworld@gmail.com>
2022-12-31 15:41:06 +1000

    fix: optimize UI

--- AcademicVisitUI/src/Header.js
+++ AcademicVisitUI/src/Header.js
@@ -2,12 +2,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="container-fluid sticky-top bg-dark">
-      <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-        <a
-          href="/"
-          className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none"
-        ></a>
-
+      <header className="d-flex flex-wrap align-items-center justify-content-center py-3  border-bottom">
         <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
           <li>
             <a href="#" className="nav-link px-2 link-secondary">
